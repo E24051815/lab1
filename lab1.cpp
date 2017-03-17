c80fd531388576dabeaee4310bb5600f69ed5f89
E24051815 <abc123456134@yahoo.com.tw>
2017-03-17 23:53:47 +0800

    aha

--- lab1.cpp
+++ lab1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
            int n;
            cin>>n;
-           cout<<n<<endl;
+           
 two:        if(n==1)  {
                     
                     return 0;
